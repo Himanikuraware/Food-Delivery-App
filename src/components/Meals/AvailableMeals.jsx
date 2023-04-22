baf6fd0543002a45da752f87423b34cd781ecb38
Himanikuraware <kurawarehimani26@gmail.com>
2023-04-22 14:22:09 +0530

    Add Styling to Meals

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -29,11 +31,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
-    <section className={classes.meals}>
-      <ul>{mealsList}</ul>
-    </section>
+    <Card>
+      <section className={classes.meals}>
+        <ul>{mealsList}</ul>
+      </section>
+    </Card>
   );
 };
 
