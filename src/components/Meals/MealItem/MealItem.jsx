ffc370e0dc3e277f89443f6d8d9977931fff9375
Himanikuraware <kurawarehimani26@gmail.com>
2023-04-23 14:56:05 +0530

    Using forwardRef and ref

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../../store/cart-context";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
   const price = `Rs. ${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
@@ -12,7 +22,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+// with the help of forwardRef we receive ref inside our custom component.
+
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/* ...props will make sure that the all key value pairs in this input object which we receive on props input are added as props to input */}
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
